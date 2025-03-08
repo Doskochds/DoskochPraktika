ffd5999a255e9933bf23d28ca543ec3c1073eb06
Kofr <darkstrik2000@gmail.com>
2025-03-09 00:31:30 +0200

    Dos1-20 <create routes for api>

---
+++ prpoj/routes/api.php
@@ -0,0 +1,28 @@
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FileController;
+use App\Http\Controllers\OneTimeLinkController;
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+Route::get('/files', [FileController::class, 'index']);
+Route::post('/files', [FileController::class, 'store']);
+Route::get('/files/{id}', [FileController::class, 'show']);
+Route::delete('/files/{id}', [FileController::class, 'destroy']);
+Route::get('/files/view/{id}', [FileController::class, 'view']);
+Route::get('/files/statistics', [FileController::class, 'statistics']);
+
+
+Route::post('/files/{id}/generate-links', [OneTimeLinkController::class, 'generate']);
+Route::get('/links/{token}', [OneTimeLinkController::class, 'view']);
+Route::delete('/links/{token}', [OneTimeLinkController::class, 'deleteLink']);
+Route::delete('/links/cleanup', [OneTimeLinkController::class, 'cleanUpLinks']);
+
+
+Route::post('/logout', [AuthController::class, 'logout']);
+});
